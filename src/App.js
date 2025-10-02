870345becbab94f05fce40c1a6e87b975fd2ea11
Vasudev290 <vasukesavulu@gmail.com>
2025-10-03 00:24:12 +0530

    feat: Added the service section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Hero from './Components/Hero/Hero'
+import Service from './Components/Services/Service'
 
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
       <Hero/>
+      <Service/>
     </div>
   )
 }
